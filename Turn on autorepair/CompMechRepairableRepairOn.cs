cae948ff64860817ae319e72326a2a2316a7ffc8
Lecht <lecht42@gmail.com>
2024-08-19 18:12:01 +0300

    Add project files.

---
+++ Turn on autorepair/CompMechRepairableRepairOn.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AutoRepairOn
+{
+    internal class CompProperties_MechRepairableOn : CompProperties
+    {
+        public CompProperties_MechRepairableOn()
+        {
+            compClass = typeof(CompMechRepairableOn);
+        }
+    }
+
+    internal class CompMechRepairableOn : CompMechRepairable
+    {
+        private new const bool autoRepair = true;
+
+        public new CompProperties_MechRepairableOn Props => (CompProperties_MechRepairableOn)props;
+
+        public CompMechRepairableOn() : base()
+        {
+            base.autoRepair = autoRepair;
+        }
+    }
+}
